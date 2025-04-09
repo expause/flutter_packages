0076b3f01622480d08205ce0fdde972e6ff816c3
Daniel Firu <admin@expause.com>
2025-04-09 14:12:40 +0300

    EXPAUSE-54 Decrypt videos on iOS

--- packages/video_player/video_player_avfoundation/darwin/video_player_avfoundation/Sources/video_player_avfoundation/FVPDecryptionLoaderDelegate.m
+++ packages/video_player/video_player_avfoundation/darwin/video_player_avfoundation/Sources/video_player_avfoundation/FVPDecryptionLoaderDelegate.m
@@ -8,7 +8,7 @@
 
 - (BOOL)resourceLoader:(AVAssetResourceLoader *)resourceLoader shouldWaitForLoadingOfRequestedResource:(AVAssetResourceLoadingRequest *)loadingRequest {
     NSURL *customURL = loadingRequest.request.URL;
-    NSLog(@"______________ Requested URL: %@", customURL.absoluteString);
+//    NSLog(@"______________ Requested URL: %@", customURL.absoluteString);
     
     // Modify the URL scheme back to https
     NSURLComponents *components = [NSURLComponents componentsWithURL:customURL resolvingAgainstBaseURL:NO];
@@ -26,7 +26,11 @@
     bool isSegmentRequest = !isKeyDecryptionRequest && !isMasterFileRequest;
     
     if (isSegmentRequest) {
-        NSLog(@"______________ Requested video segment. Returning NO");
+//        NSLog(@"______________ Requested video segment. Returning NO");
+        
+        NSURLRequest *redirect = [NSURLRequest requestWithURL:actualUrl];
+        loadingRequest.redirect = redirect;
+        [loadingRequest finishLoading];
         return NO;
     }
     
@@ -47,63 +51,58 @@
     }
     
     if (isKeyDecryptionRequest) {
-        NSLog(@"______________ Requested hls key");
-        // Respond with decryption key
-        if (key) {
-            //            loadingRequest.contentInformationRequest.contentType = AVStreamingKeyDeliveryContentKeyType;
-            loadingRequest.contentInformationRequest.contentType = AVStreamingKeyDeliveryPersistentContentKeyType;
-            loadingRequest.contentInformationRequest.byteRangeAccessSupported = YES;
-            loadingRequest.contentInformationRequest.contentLength = key.length;
-            [loadingRequest.dataRequest respondWithData:key];
-            [loadingRequest finishLoading];
-            return YES;
-        }
+//        NSLog(@"______________ Requested hls key");
+        loadingRequest.contentInformationRequest.contentType = AVStreamingKeyDeliveryPersistentContentKeyType;
+        loadingRequest.contentInformationRequest.byteRangeAccessSupported = YES;
+        loadingRequest.contentInformationRequest.contentLength = key.length;
+        [loadingRequest.dataRequest respondWithData:key];
+        [loadingRequest finishLoading];
+        return YES;
     }
     
     if (isMasterFileRequest) {
-        NSLog(@"______________ Requested master file");
+//        NSLog(@"______________ Requested master file");
         
-        // Decrypt both master and variant playlist
-        NSData *encryptedData = [NSData dataWithContentsOfURL:actualUrl];
-        if (!encryptedData) {
-            NSLog(@"_______====== Failed to download encrypted data");
-            [loadingRequest finishLoadingWithError:[NSError errorWithDomain:@"com.yourapp.video" code:-1 userInfo:@{NSLocalizedDescriptionKey: @"Failed to download encrypted data"}]];
-            return NO;
-        }
-        
-        NSData *iv = [[EncryptedVideoManager sharedInstance] getVideoIvKey:videoId];
-        
-        NSData *decryptedData = [self decryptData:encryptedData withKey:key withIv:iv];
-        if (!decryptedData) {
-            NSLog(@"_______====== Data decryption failed");
-            NSError *decryptError = [NSError errorWithDomain:@"com.yourapp.video" code:-1 userInfo:@{NSLocalizedDescriptionKey: @"Decryption failed"}];
-            [loadingRequest finishLoadingWithError:decryptError];
-            return NO;
-        }
-        
-        NSMutableString *masterFile = [[NSMutableString alloc] initWithData:decryptedData encoding:NSUTF8StringEncoding];
-        
-        if (isMasterPlaylistFileRequest) {
-            NSLog(@"______________ Is Playlist. Replacing IV");
-            
-            // Replace IV for playlist file only
-            const unsigned char *ivBytes = (const unsigned char *)[iv bytes];
-            NSMutableString *ivHex = [NSMutableString stringWithString:@"0x"];
-            for (int i = 0; i < iv.length; i++) {
-                [ivHex appendFormat:@"%02x", ivBytes[i]];
+        NSURLSession *session = [NSURLSession sharedSession];
+        NSURLSessionDataTask *task = [session dataTaskWithURL:actualUrl completionHandler:^(NSData * _Nullable encryptedData, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+            if (!encryptedData || error) {
+                NSLog(@"_______====== Failed to download encrypted data");
+                [loadingRequest finishLoadingWithError:error ?: [NSError errorWithDomain:@"com.yourapp.video" code:-1 userInfo:@{NSLocalizedDescriptionKey: @"Failed to download encrypted data"}]];
+                return;
             }
             
-            NSLog(@"IV for videoId %@: %@", videoId, ivHex);
+            NSData *iv = [[EncryptedVideoManager sharedInstance] getVideoIvKey:videoId];
+            NSData *decryptedData = [self decryptData:encryptedData withKey:key withIv:iv];
+            if (!decryptedData) {
+                NSLog(@"_______====== Data decryption failed");
+                NSError *decryptError = [NSError errorWithDomain:@"com.yourapp.video" code:-1 userInfo:@{NSLocalizedDescriptionKey: @"Decryption failed"}];
+                [loadingRequest finishLoadingWithError:decryptError];
+                return;
+            }
             
-            NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"IV=0x[0-9a-fA-F]{32}" options:0 error:nil];
-            NSString *updatedPlaylist = [regex stringByReplacingMatchesInString:masterFile options:0 range:NSMakeRange(0, masterFile.length) withTemplate:[NSString stringWithFormat:@"IV=%@", ivHex]];
-            masterFile = [updatedPlaylist mutableCopy];
-        }
+            NSMutableString *masterFile = [[NSMutableString alloc] initWithData:decryptedData encoding:NSUTF8StringEncoding];
+            
+            if (isMasterPlaylistFileRequest) {
+//                NSLog(@"______________ Is Playlist. Replacing IV");
+                
+                const unsigned char *ivBytes = (const unsigned char *)[iv bytes];
+                NSMutableString *ivHex = [NSMutableString stringWithString:@"0x"];
+                for (int i = 0; i < iv.length; i++) {
+                    [ivHex appendFormat:@"%02x", ivBytes[i]];
+                }
+                
+                NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"IV=0x[0-9a-fA-F]{32}" options:0 error:nil];
+                NSString *updatedPlaylist = [regex stringByReplacingMatchesInString:masterFile options:0 range:NSMakeRange(0, masterFile.length) withTemplate:[NSString stringWithFormat:@"IV=%@", ivHex]];
+                masterFile = [updatedPlaylist mutableCopy];
+            }
+            
+            NSData *finalData = [masterFile dataUsingEncoding:NSUTF8StringEncoding];
+            [loadingRequest.dataRequest respondWithData:finalData];
+            [loadingRequest finishLoading];
+        }];
         
-        NSData *finalData = [masterFile dataUsingEncoding:NSUTF8StringEncoding];
-        [loadingRequest.dataRequest respondWithData:finalData];
-        [loadingRequest finishLoading];
-        return YES;
+        [task resume];
+        return YES; // We are handling the loading request asynchronously.
     }
     
     return NO;
